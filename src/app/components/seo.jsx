7d217e5800ca0bc26c005423b56c57109dd0a244
PL-Kenshin <matpt222@gmail.com>
2024-09-03 14:53:51 +0200

    SEO first commit

--- src/app/components/seo.jsx
+++ src/app/components/seo.jsx
@@ -1,7 +1,16 @@
 const SEO = () => {
     return (
-        <div>
-
+        <div className="flex justify-around bg-neutral-800 text-white py-8">
+            <div className="w-1/2">
+                <h1 className="text-xl uppercase font-bold">Lorem Ipsum.</h1>
+                <p className="line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed lacus non ex posuere fringilla. Cras nibh leo, sagittis vel est ac, dignissim dapibus nisl. Donec ut leo urna. Maecenas porta metus in commodo mattis. Maecenas pharetra vehicula lectus sit amet sagittis. Donec aliquam nibh eget commodo maximus. Nulla facilisi. Fusce id ipsum viverra, pellentesque mi id, sagittis metus. Phasellus posuere ac magna sit amet gravida. Sed accumsan consectetur lacus, nec pharetra dolor vehicula nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae sagittis velit. Nam condimentum egestas arcu eleifend facilisis. Suspendisse egestas, nibh vitae auctor imperdiet, felis magna aliquam elit, in finibus nibh est et metus. Curabitur iaculis purus ante. Integer ut felis eu tellus dictum fringilla a ut leo.</p>
+                <button className="border-b">Rozwiń</button>
+            </div>
+            <div className="w-1/2">
+                <h1 className="text-xl uppercase font-bold">Lorem Ipsum.</h1>
+                <p className="line-clamp-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed lacus non ex posuere fringilla. Cras nibh leo, sagittis vel est ac, dignissim dapibus nisl. Donec ut leo urna. Maecenas porta metus in commodo mattis. Maecenas pharetra vehicula lectus sit amet sagittis. Donec aliquam nibh eget commodo maximus. Nulla facilisi. Fusce id ipsum viverra, pellentesque mi id, sagittis metus. Phasellus posuere ac magna sit amet gravida. Sed accumsan consectetur lacus, nec pharetra dolor vehicula nec. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae sagittis velit. Nam condimentum egestas arcu eleifend facilisis. Suspendisse egestas, nibh vitae auctor imperdiet, felis magna aliquam elit, in finibus nibh est et metus. Curabitur iaculis purus ante. Integer ut felis eu tellus dictum fringilla a ut leo.</p>
+                <button className="border-b">Rozwiń</button>
+            </div>
         </div>
     )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import SEO from "./components/seo";
 
 export const metadata = {
   title: "Create Next App",
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
       <body>
         <Navbar/>
         {children}
+        <SEO/>
         <Footer/>
       </body>
     </html>
