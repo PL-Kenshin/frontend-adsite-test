9386b9553b63df709b86f9ed51e358ce9e88f99f
PL-Kenshin <matpt222@gmail.com>
2024-09-04 13:23:16 +0200

    main page

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
                         <Link href="/" className='mx-2'>FaQ</Link>
                     </div>
                     <div className='flex w-1/2 justify-center'>
-                        <button className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Zadzwoń do nas</button>
+                        <button className='px-6 py-3 text-white bg-blue-600 rounded-md hover:bg-blue-700'>Zadzwoń do nas</button>
                     </div>
             </div>
         </nav>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,28 @@
 export default function Home() {
   return (
     <main>
-      
+      <div class="relative flex items-center justify-between p-10">
+        <div class="max-w-xl p-10">
+          <h1 class="text-5xl font-bold text-gray-900 tracking-tighter">
+            SPRZEDAJEMY SAMOCHODY Z EUROPY
+          </h1>
+          <p class="mt-4 text-lg text-gray-600">
+            Kup komfortowy pojazd, aby każda podróż była wyjątkowym przeżyciem.
+          </p>
+          <div class="mt-8 flex space-x-4">
+            <button class="px-6 py-3 text-white bg-blue-600 rounded-md hover:bg-blue-700">
+              Zobacz zdjęcia
+            </button>
+            <button class="px-6 py-3 text-blue-600 border border-blue-600 rounded-md hover:bg-blue-50">
+              Zadzwoń do nas
+            </button>
+          </div>
+        </div>
+
+        <div class="absolute right-0 bottom-0 max-w-3xl -z-10">
+          <img src="/cars.png" alt="Samochody" class="object-cover"/>
+        </div>
+      </div>
     </main>
   );
 }
