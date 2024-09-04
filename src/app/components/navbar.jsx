ba3c8d699b9f4d5d8a125244b0541d194c6ea8d2
PL-Kenshin <matpt222@gmail.com>
2024-09-04 12:37:16 +0200

    change button behavior

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
                         <Link href="/" className='mx-2'>FaQ</Link>
                     </div>
                     <div className='flex w-1/2 justify-center'>
-                        <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Zadzwoń do nas</button>
+                        <button className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Zadzwoń do nas</button>
                     </div>
             </div>
         </nav>
